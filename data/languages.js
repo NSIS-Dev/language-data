876c0a29224e70525d3a56005442e2cf6160ad91
idleberg <jan@fragnebenan.com>
2018-08-03 15:48:06 +0200

    add code_page and rtl

--- data/languages.js
+++ data/languages.js
@@ -1,309 +1,439 @@
 module.exports = {
     "Afrikaans": {
         "id": 1078,
+        "code_page": 1052,
+        "rtl": false,
         "native": "Afrikaans"
     },
     "Albanian": {
         "id": 1052,
+        "code_page": 1250,
+        "rtl": false,
         "native": "Shqip"
     },
     "Arabic": {
         "id": 1025,
+        "code_page": 1256,
+        "rtl": true,
         "native": "العربية",
         "literal": "Al-Arabiyyah"
     },
     "Armenian": {
         "id": 1067,
+        "code_page": 1200,
+        "rtl": false,
         "native": "Հայերեն",
         "literal": "Hayeren"
     },
     "Asturian": {
         "id": 9997,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Asturies"
     },
     "Basque": {
         "id": 1069,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Euskera"
     },
     "Belarusian": {
         "id": 1059,
+        "code_page": 1251,
+        "rtl": false,
         "native": "Беларуская",
         "literal": "Bielaruskaja"
     },
     "Bosnian": {
         "id": 5146,
+        "code_page": 1250,
+        "rtl": false,
         "native": "Bosanski"
     },
     "Breton": {
         "id": 1150,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Brezhoneg"
     },
     "Bulgarian": {
         "id": 1026,
+        "code_page": 1251,
+        "rtl": false,
         "native": "Български",
         "literal": "Balgarski"
     },
     "Catalan": {
         "id": 1027,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Català",
         "literal": "Catala"
     },
     "Corsican": {
         "id": 1155,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Corsu"
     },
     "Croatian": {
         "id": 1050,
+        "code_page": 1250,
+        "rtl": false,
         "native": "Hrvatski"
     },
     "Czech": {
         "id": 1029,
+        "code_page": 1250,
+        "rtl": false,
         "native": "Čeština"
     },
     "Danish": {
         "id": 1030,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Dansk"
     },
     "Dutch": {
         "id": 1043,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Nederlands"
     },
     "English": {
         "id": 1033,
+        "code_page": "-",
+        "rtl": false,
         "native": "English"
     },
     "Esperanto": {
         "id": 9998,
+        "code_page": "-",
+        "rtl": false,
         "native": "Esperanto"
     },
     "Estonian": {
         "id": 1061,
+        "code_page": 1257,
+        "rtl": false,
         "native": "Eesti keel"
     },
     "Farsi": {
         "id": 1065,
+        "code_page": 1256,
+        "rtl": true,
         "long": "Persian",
         "native": "فارسی",
         "literal": "Farsi"
     },
     "Finnish": {
         "id": 1035,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Suomi"
     },
     "French": {
         "id": 1036,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Français",
         "literal": "Francais"
     },
     "Galician": {
         "id": 1110,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Galego"
     },
     "Georgian": {
         "id": 1079,
+        "code_page": 1200,
+        "rtl": false,
         "native": "ქართული",
         "literal": "Kartuli"
     },
     "German": {
         "id": 1031,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Deutsch"
     },
     "Greek": {
         "id": 1032,
+        "code_page": 1253,
+        "rtl": false,
         "native": "Ελληνικά",
         "literal": "Ellinika"
     },
     "Hebrew": {
         "id": 1037,
+        "code_page": 1255,
+        "rtl": true,
         "native": "עברית",
         "literal": "Ivrit"
     },
     "Hungarian": {
         "id": 1038,
+        "code_page": 1250,
+        "rtl": false,
         "native": "Magyar"
     },
     "Icelandic": {
         "id": 1039,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Íslenska",
         "literal": "Islenska"
     },
     "Indonesian": {
         "id": 1057,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Bahasa Indonesia"
     },
     "Irish": {
         "id": 2108,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Gaeilge"
     },
     "Italian": {
         "id": 1040,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Italiano"
     },
     "Japanese": {
         "id": 1041,
+        "code_page": 932,
+        "rtl": false,
         "native": "日本語",
         "literal": "Nihongo"
     },
     "Korean": {
         "id": 1042,
+        "code_page": 949,
+        "rtl": false,
         "native": "한국어",
         "literal": "Hangugeo"
     },
     "Kurdish": {
         "id": 9999,
+        "code_page": 1254,
+        "rtl": false,
         "native": "Kurdî",
         "literal": "Kurdi"
     },
     "Latvian": {
         "id": 1062,
+        "code_page": 1257,
+        "rtl": false,
         "native": "Latviešu",
         "literal": "Latviesu"
     },
     "Lithuanian": {
         "id": 1063,
+        "code_page": 1063,
+        "rtl": false,
         "native": "Lietuvių",
         "literal": "Lietuviu"
     },
     "Luxembourgish": {
         "id": 4103,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Lëtzebuergesch",
         "literal": "Letzebuergesch"
     },
     "Macedonian": {
         "id": 1071,
+        "code_page": 1251,
+        "rtl": false,
         "native": "Македонски",
         "literal": "Makedonski"
     },
     "Malay": {
         "id": 1086,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Bahasa Melayu"
     },
     "Mongolian": {
         "id": 1104,
+        "code_page": 1251,
+        "rtl": false,
         "long": "Mongolian (Cyrillic)",
         "native": "Монгол Кирилл",
         "literal": "Mongol kirill"
     },
     "Norwegian": {
         "id": 1044,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Norsk"
     },
     "NorwegianNynorsk": {
         "id": 2068,
+        "code_page": 1252,
+        "rtl": false,
         "long": "Norwegian (Nynorsk)",
         "native":  "Norsk (nynorsk)"
     },
     "Pashto": {
         "id": 1123,
+        "code_page": 1256,
+        "rtl": true,
         "native": "پښتو",
         "literal": "Pashto"
     },
     "Polish": {
         "id": 1045,
+        "code_page": 1045,
+        "rtl": false,
         "native": "Polski"
     },
     "Portuguese": {
         "id": 2070,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Português",
         "literal": "Portugues"
     },
     "PortugueseBR": {
         "id": 1046,
+        "code_page": 1252,
+        "rtl": false,
         "long": "Brazilian Portuguese",
         "native": "Português Brasileiro",
         "literal": "Portugues Brasileiro"
     },
     "Romanian": {
         "id": 1048,
+        "code_page": 1250,
+        "rtl": false,
         "native": "Română",
         "literal": "Romana"
     },
     "Russian": {
         "id": 1049,
+        "code_page": 1251,
+        "rtl": false,
         "native": "Русский",
         "literal": "Russkij"
     },
     "ScotsGaelic": {
         "id": 1169,
+        "code_page": 1252,
+        "rtl": false,
         "long": "Scottish Gaelic",
         "native": "Gàidhlig",
         "literal": "Gaidhlig"
     },
     "Serbian": {
         "id": 3098,
+        "code_page": 1251,
+        "rtl": false,
         "long": "Serbian (Cyrillic)",
         "native": "Српски",
         "literal": "Srpski (Cyrillic)"
     },
     "SerbianLatin": {
         "id": 2074,
+        "code_page": 1250,
+        "rtl": false,
         "long": "Serbian (Latin)",
         "literal": "Srpski"
     },
     "SimpChinese": {
         "id": 2052,
+        "code_page": 936,
+        "rtl": false,
         "long": "Chinese (Simplified)",
         "native": "中文(简体)",
         "literal": "Hanyu (Jiantizi)"
     },
     "Slovak": {
         "id": 1051,
+        "code_page": 1250,
+        "rtl": false,
         "native": "Slovenčina",
         "literal": "Slovencina"
     },
     "Slovenian": {
         "id": 1060,
+        "code_page": 1250,
+        "rtl": false,
         "native": "Slovenski"
     },
     "Spanish": {
         "id": 1034,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Español",
         "literal": "Espanol"
     },
     "SpanishInternational": {
         "id": 3082,
+        "code_page": 1252,
+        "rtl": false,
         "long": "Spanish (International)",
         "native": "Español (Alfabetización Internacional)",
         "literal": "Espanol (Alfabetizacion Internacional)"
     },
     "Tatar": {
         "id": 1092,
+        "code_page": 1251,
+        "rtl": false,
         "native": "Татарча",
         "literal": "Tatarcha"
     },
     "Thai": {
         "id": 1054,
+        "code_page": 874,
+        "rtl": false,
         "native": "ไทย",
         "literal": "Thai"
     },
     "TradChinese": {
         "id": 1028,
+        "code_page": 950,
+        "rtl": false,
         "long": "Chinese (Traditional)",
         "native": "中文(繁體)",
         "literal": "Hanyu (Fantizi)"
     },
     "Turkish": {
         "id": 1055,
+        "code_page": 1254,
+        "rtl": false,
         "native": "Türkçe",
         "literal": "Turkce"
     },
     "Ukrainian": {
         "id": 1058,
+        "code_page": 1251,
+        "rtl": false,
         "native": "Українська",
         "literal": "Ukrayins'ka"
     },
     "Uzbek": {
         "id": 1091,
+        "code_page": 1252,
+        "rtl": false,
         "native": "O‘zbek",
         "literal": "O'zbek"
     },
     "Vietnamese": {
         "id": 1066,
+        "code_page": 1258,
+        "rtl": false,
         "native": "Tiếng Việt",
         "literal": "Tieng Viet"
     },
     "Welsh": {
         "id": 1106,
+        "code_page": 1252,
+        "rtl": false,
         "native": "Cymraeg"
     }
 };
