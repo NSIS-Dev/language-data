ccfde41c0d3ae194a8fbfca6ec449a8775823c13
idleberg <jan@idleberg.com>
2018-09-17 19:05:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,145 @@
+/**
+ * Language Meta Data
+ */
+const meta = require("./data/meta.js");
+
+module.exports.meta = meta;
+
+/**
+ * Language Data
+ */
+const Afrikaans = require("./data/languages/Afrikaans.js");
+const Albanian = require("./data/languages/Albanian.js");
+const Arabic = require("./data/languages/Arabic.js");
+const Armenian = require("./data/languages/Armenian.js");
+const Asturian = require("./data/languages/Asturian.js");
+const Basque = require("./data/languages/Basque.js");
+const Belarusian = require("./data/languages/Belarusian.js");
+const Bosnian = require("./data/languages/Bosnian.js");
+const Breton = require("./data/languages/Breton.js");
+const Bulgarian = require("./data/languages/Bulgarian.js");
+const Catalan = require("./data/languages/Catalan.js");
+const Corsican = require("./data/languages/Corsican.js");
+const Croatian = require("./data/languages/Croatian.js");
+const Czech = require("./data/languages/Czech.js");
+const Danish = require("./data/languages/Danish.js");
+const Dutch = require("./data/languages/Dutch.js");
+const English = require("./data/languages/English.js");
+const Esperanto = require("./data/languages/Esperanto.js");
+const Estonian = require("./data/languages/Estonian.js");
+const Farsi = require("./data/languages/Farsi.js");
+const Finnish = require("./data/languages/Finnish.js");
+const French = require("./data/languages/French.js");
+const Galician = require("./data/languages/Galician.js");
+const Georgian = require("./data/languages/Georgian.js");
+const German = require("./data/languages/German.js");
+const Greek = require("./data/languages/Greek.js");
+const Hebrew = require("./data/languages/Hebrew.js");
+const Hungarian = require("./data/languages/Hungarian.js");
+const Icelandic = require("./data/languages/Icelandic.js");
+const Indonesian = require("./data/languages/Indonesian.js");
+const Irish = require("./data/languages/Irish.js");
+const Italian = require("./data/languages/Italian.js");
+const Japanese = require("./data/languages/Japanese.js");
+const Korean = require("./data/languages/Korean.js");
+const Kurdish = require("./data/languages/Kurdish.js");
+const Latvian = require("./data/languages/Latvian.js");
+const Lithuanian = require("./data/languages/Lithuanian.js");
+const Luxembourgish = require("./data/languages/Luxembourgish.js");
+const Macedonian = require("./data/languages/Macedonian.js");
+const Malay = require("./data/languages/Malay.js");
+const Mongolian = require("./data/languages/Mongolian.js");
+const Norwegian = require("./data/languages/Norwegian.js");
+const NorwegianNynorsk = require("./data/languages/NorwegianNynorsk.js");
+const Pashto = require("./data/languages/Pashto.js");
+const Polish = require("./data/languages/Polish.js");
+const Portuguese = require("./data/languages/Portuguese.js");
+const PortugueseBR = require("./data/languages/PortugueseBR.js");
+const Romanian = require("./data/languages/Romanian.js");
+const Russian = require("./data/languages/Russian.js");
+const ScotsGaelic = require("./data/languages/ScotsGaelic.js");
+const Serbian = require("./data/languages/Serbian.js");
+const SerbianLatin = require("./data/languages/SerbianLatin.js");
+const SimpChinese = require("./data/languages/SimpChinese.js");
+const Slovak = require("./data/languages/Slovak.js");
+const Slovenian = require("./data/languages/Slovenian.js");
+const Spanish = require("./data/languages/Spanish.js");
+const SpanishInternational = require("./data/languages/SpanishInternational.js");
+const Swedish = require("./data/languages/Swedish.js");
+const Tatar = require("./data/languages/Tatar.js");
+const Thai = require("./data/languages/Thai.js");
+const TradChinese = require("./data/languages/TradChinese.js");
+const Turkish = require("./data/languages/Turkish.js");
+const Ukrainian = require("./data/languages/Ukrainian.js");
+const Uzbek = require("./data/languages/Uzbek.js");
+const Vietnamese = require("./data/languages/Vietnamese.js");
+const Welsh = require("./data/languages/Welsh.js");
+
+module.exports.language = {
+  Afrikaans: Afrikaans,
+  Albanian: Albanian,
+  Arabic: Arabic,
+  Armenian: Armenian,
+  Asturian: Asturian,
+  Basque: Basque,
+  Belarusian: Belarusian,
+  Bosnian: Bosnian,
+  Breton: Breton,
+  Bulgarian: Bulgarian,
+  Catalan: Catalan,
+  Corsican: Corsican,
+  Croatian: Croatian,
+  Czech: Czech,
+  Danish: Danish,
+  Dutch: Dutch,
+  English: English,
+  Esperanto: Esperanto,
+  Estonian: Estonian,
+  Farsi: Farsi,
+  Finnish: Finnish,
+  French: French,
+  Galician: Galician,
+  Georgian: Georgian,
+  German: German,
+  Greek: Greek,
+  Hebrew: Hebrew,
+  Hungarian: Hungarian,
+  Icelandic: Icelandic,
+  Indonesian: Indonesian,
+  Irish: Irish,
+  Italian: Italian,
+  Japanese: Japanese,
+  Korean: Korean,
+  Kurdish: Kurdish,
+  Latvian: Latvian,
+  Lithuanian: Lithuanian,
+  Luxembourgish: Luxembourgish,
+  Macedonian: Macedonian,
+  Malay: Malay,
+  Mongolian: Mongolian,
+  Norwegian: Norwegian,
+  NorwegianNynorsk: NorwegianNynorsk,
+  Pashto: Pashto,
+  Polish: Polish,
+  Portuguese: Portuguese,
+  PortugueseBR: PortugueseBR,
+  Romanian: Romanian,
+  Russian: Russian,
+  ScotsGaelic: ScotsGaelic,
+  Serbian: Serbian,
+  SerbianLatin: SerbianLatin,
+  SimpChinese: SimpChinese,
+  Slovak: Slovak,
+  Slovenian: Slovenian,
+  Spanish: Spanish,
+  SpanishInternational: SpanishInternational,
+  Swedish: Swedish,
+  Tatar: Tatar,
+  Thai: Thai,
+  TradChinese: TradChinese,
+  Turkish: Turkish,
+  Ukrainian: Ukrainian,
+  Uzbek: Uzbek,
+  Vietnamese: Vietnamese,
+  Welsh: Welsh
+};
