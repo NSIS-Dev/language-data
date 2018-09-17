774713d6f5b7f49c3ce4f13475b2dba098ad654d
idleberg <jan@idleberg.com>
2018-09-17 18:09:40 +0200

    modify file pattern

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const jsonlint = require('gulp-jsonlint');
 
 // Lint JavaScript files
 gulp.task('eslint', (done) => {
-  gulp.src(['./data/languages.js'])
+  gulp.src(['./data/**/*.js'])
   .pipe(debug({title: 'eslint:'}))
   .pipe(eslint())
   .pipe(eslint.failAfterError());
@@ -16,7 +16,7 @@ gulp.task('eslint', (done) => {
 
 // Lint JSON files
 gulp.task('jsonlint', (done) => {
-  gulp.src(['./data/languages.json'])
+  gulp.src(['./data/**/*.json'])
   .pipe(debug({title: 'jsonlint:'}))
   .pipe(jsonlint())
   .pipe(jsonlint.reporter());
