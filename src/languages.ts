fad0afda8c9c96c8955bde6f9769be5e468ce0dd
Jan T. Sott <git@idleberg.com>
2026-03-19 19:15:53 +0100

    chore: improve treeshake-ability

---
+++ src/languages.ts
@@ -0,0 +1,208 @@
+import { default as Afrikaans } from '../data/languages/Afrikaans.json' with { type: 'json' };
+import { default as Albanian } from '../data/languages/Albanian.json' with { type: 'json' };
+import { default as Arabic } from '../data/languages/Arabic.json' with { type: 'json' };
+import { default as Armenian } from '../data/languages/Armenian.json' with { type: 'json' };
+import { default as Asturian } from '../data/languages/Asturian.json' with { type: 'json' };
+import { default as Basque } from '../data/languages/Basque.json' with { type: 'json' };
+import { default as Belarusian } from '../data/languages/Belarusian.json' with { type: 'json' };
+import { default as Bosnian } from '../data/languages/Bosnian.json' with { type: 'json' };
+import { default as Breton } from '../data/languages/Breton.json' with { type: 'json' };
+import { default as Bulgarian } from '../data/languages/Bulgarian.json' with { type: 'json' };
+import { default as Catalan } from '../data/languages/Catalan.json' with { type: 'json' };
+import { default as Corsican } from '../data/languages/Corsican.json' with { type: 'json' };
+import { default as Croatian } from '../data/languages/Croatian.json' with { type: 'json' };
+import { default as Czech } from '../data/languages/Czech.json' with { type: 'json' };
+import { default as Danish } from '../data/languages/Danish.json' with { type: 'json' };
+import { default as Dutch } from '../data/languages/Dutch.json' with { type: 'json' };
+import { default as English } from '../data/languages/English.json' with { type: 'json' };
+import { default as Esperanto } from '../data/languages/Esperanto.json' with { type: 'json' };
+import { default as Estonian } from '../data/languages/Estonian.json' with { type: 'json' };
+import { default as Farsi } from '../data/languages/Farsi.json' with { type: 'json' };
+import { default as Finnish } from '../data/languages/Finnish.json' with { type: 'json' };
+import { default as French } from '../data/languages/French.json' with { type: 'json' };
+import { default as Galician } from '../data/languages/Galician.json' with { type: 'json' };
+import { default as Georgian } from '../data/languages/Georgian.json' with { type: 'json' };
+import { default as German } from '../data/languages/German.json' with { type: 'json' };
+import { default as Greek } from '../data/languages/Greek.json' with { type: 'json' };
+import { default as Hebrew } from '../data/languages/Hebrew.json' with { type: 'json' };
+import { default as Hindi } from '../data/languages/Hindi.json' with { type: 'json' };
+import { default as Hungarian } from '../data/languages/Hungarian.json' with { type: 'json' };
+import { default as Icelandic } from '../data/languages/Icelandic.json' with { type: 'json' };
+import { default as Indonesian } from '../data/languages/Indonesian.json' with { type: 'json' };
+import { default as Irish } from '../data/languages/Irish.json' with { type: 'json' };
+import { default as Italian } from '../data/languages/Italian.json' with { type: 'json' };
+import { default as Japanese } from '../data/languages/Japanese.json' with { type: 'json' };
+import { default as Korean } from '../data/languages/Korean.json' with { type: 'json' };
+import { default as Kurdish } from '../data/languages/Kurdish.json' with { type: 'json' };
+import { default as Latvian } from '../data/languages/Latvian.json' with { type: 'json' };
+import { default as Lithuanian } from '../data/languages/Lithuanian.json' with { type: 'json' };
+import { default as Luxembourgish } from '../data/languages/Luxembourgish.json' with { type: 'json' };
+import { default as Macedonian } from '../data/languages/Macedonian.json' with { type: 'json' };
+import { default as Malay } from '../data/languages/Malay.json' with { type: 'json' };
+import { default as Mongolian } from '../data/languages/Mongolian.json' with { type: 'json' };
+import { default as Norwegian } from '../data/languages/Norwegian.json' with { type: 'json' };
+import { default as NorwegianNynorsk } from '../data/languages/NorwegianNynorsk.json' with { type: 'json' };
+import { default as Pashto } from '../data/languages/Pashto.json' with { type: 'json' };
+import { default as Polish } from '../data/languages/Polish.json' with { type: 'json' };
+import { default as Portuguese } from '../data/languages/Portuguese.json' with { type: 'json' };
+import { default as PortugueseBR } from '../data/languages/PortugueseBR.json' with { type: 'json' };
+import { default as Romanian } from '../data/languages/Romanian.json' with { type: 'json' };
+import { default as Russian } from '../data/languages/Russian.json' with { type: 'json' };
+import { default as ScotsGaelic } from '../data/languages/ScotsGaelic.json' with { type: 'json' };
+import { default as Serbian } from '../data/languages/Serbian.json' with { type: 'json' };
+import { default as SerbianLatin } from '../data/languages/SerbianLatin.json' with { type: 'json' };
+import { default as SimpChinese } from '../data/languages/SimpChinese.json' with { type: 'json' };
+import { default as Slovak } from '../data/languages/Slovak.json' with { type: 'json' };
+import { default as Slovenian } from '../data/languages/Slovenian.json' with { type: 'json' };
+import { default as Spanish } from '../data/languages/Spanish.json' with { type: 'json' };
+import { default as SpanishInternational } from '../data/languages/SpanishInternational.json' with { type: 'json' };
+import { default as Swedish } from '../data/languages/Swedish.json' with { type: 'json' };
+import { default as Tatar } from '../data/languages/Tatar.json' with { type: 'json' };
+import { default as Thai } from '../data/languages/Thai.json' with { type: 'json' };
+import { default as TradChinese } from '../data/languages/TradChinese.json' with { type: 'json' };
+import { default as Turkish } from '../data/languages/Turkish.json' with { type: 'json' };
+import { default as Ukrainian } from '../data/languages/Ukrainian.json' with { type: 'json' };
+import { default as Uzbek } from '../data/languages/Uzbek.json' with { type: 'json' };
+import { default as Vietnamese } from '../data/languages/Vietnamese.json' with { type: 'json' };
+import { default as Welsh } from '../data/languages/Welsh.json' with { type: 'json' };
+
+const languages = {
+	Afrikaans,
+	Albanian,
+	Arabic,
+	Armenian,
+	Asturian,
+	Basque,
+	Belarusian,
+	Bosnian,
+	Breton,
+	Bulgarian,
+	Catalan,
+	Corsican,
+	Croatian,
+	Czech,
+	Danish,
+	Dutch,
+	English,
+	Esperanto,
+	Estonian,
+	Farsi,
+	Finnish,
+	French,
+	Galician,
+	Georgian,
+	German,
+	Greek,
+	Hebrew,
+	Hindi,
+	Hungarian,
+	Icelandic,
+	Indonesian,
+	Irish,
+	Italian,
+	Japanese,
+	Korean,
+	Kurdish,
+	Latvian,
+	Lithuanian,
+	Luxembourgish,
+	Macedonian,
+	Malay,
+	Mongolian,
+	Norwegian,
+	NorwegianNynorsk,
+	Pashto,
+	Polish,
+	Portuguese,
+	PortugueseBR,
+	Romanian,
+	Russian,
+	ScotsGaelic,
+	Serbian,
+	SerbianLatin,
+	SimpChinese,
+	Slovak,
+	Slovenian,
+	Spanish,
+	SpanishInternational,
+	Swedish,
+	Tatar,
+	Thai,
+	TradChinese,
+	Turkish,
+	Ukrainian,
+	Uzbek,
+	Vietnamese,
+	Welsh,
+};
+
+export {
+	Afrikaans,
+	Albanian,
+	Arabic,
+	Armenian,
+	Asturian,
+	Basque,
+	Belarusian,
+	Bosnian,
+	Breton,
+	Bulgarian,
+	Catalan,
+	Corsican,
+	Croatian,
+	Czech,
+	Danish,
+	Dutch,
+	English,
+	Esperanto,
+	Estonian,
+	Farsi,
+	Finnish,
+	French,
+	Galician,
+	Georgian,
+	German,
+	Greek,
+	Hebrew,
+	Hindi,
+	Hungarian,
+	Icelandic,
+	Indonesian,
+	Irish,
+	Italian,
+	Japanese,
+	Korean,
+	Kurdish,
+	Latvian,
+	Lithuanian,
+	Luxembourgish,
+	languages,
+	Macedonian,
+	Malay,
+	Mongolian,
+	Norwegian,
+	NorwegianNynorsk,
+	Pashto,
+	Polish,
+	Portuguese,
+	PortugueseBR,
+	Romanian,
+	Russian,
+	ScotsGaelic,
+	Serbian,
+	SerbianLatin,
+	SimpChinese,
+	Slovak,
+	Slovenian,
+	Spanish,
+	SpanishInternational,
+	Swedish,
+	Tatar,
+	Thai,
+	TradChinese,
+	Turkish,
+	Ukrainian,
+	Uzbek,
+	Vietnamese,
+	Welsh,
+};
---
+++ src/meta.ts
@@ -0,0 +1,1 @@
+export { default as meta } from '../data/meta.json';
