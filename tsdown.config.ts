73e6e8705fba4a26f919c9deb02a904f18cc352b
Jan T. Sott <git@idleberg.com>
2026-03-19 21:31:50 +0100

    chore: update build config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
 	clean: true,
-	deps: {
-		alwaysBundle: ['@nsis/dent', '@nsis/nlf', 'makensis', 'micromatch', 'open', 'vscode-get-config', 'which'],
-		neverBundle: ['vscode'],
-		onlyBundle: false,
-	},
+	dts: true,
 	entry: {
 		index: 'src/index.ts',
 		codepages: 'src/codepages.ts',
