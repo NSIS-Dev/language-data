8935e5ad6c8233450d4de42b047f1d7da381ff7c
Jan T. Sott <git@idleberg.com>
2026-03-19 19:15:08 +0100

    chore: update tooling

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,11 @@
 /* eslint-disable */
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es6: true
-},
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  plugins: [
-    '@typescript-eslint'
-  ],
-  rules: {
-  }
+	root: true,
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	plugins: ['@typescript-eslint'],
+	rules: {},
 };
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,49 +2,53 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
 
-const plugins = [
-  commonjs(),
-  json()
-];
+const plugins = [commonjs(), json()];
 
 const compilerOptions = {
-  allowSyntheticDefaultImports: true,
-  moduleResolution: "node",
-  resolveJsonModule: true,
-  strictNullChecks: true,
-  typeRoots: ['./types', './node_modules/@types']
+	allowSyntheticDefaultImports: true,
+	moduleResolution: 'node',
+	resolveJsonModule: true,
+	strictNullChecks: true,
+	typeRoots: ['./types', './node_modules/@types'],
 };
 
-const external = [
-];
+const external = [];
+
+const input = {
+	index: 'src/index.ts',
+	codepages: 'src/codepages.ts',
+	meta: 'src/meta.ts',
+	languages: 'src/languages.ts',
+};
 
 export default [
-  {
-    external,
-    input: 'src/index.ts',
-    output: {
-      file: 'dist/index.cjs',
-      format: 'cjs'
-    },
-    plugins: [
-      ...plugins,
-      typescript(compilerOptions)
-    ]
-  },
-  {
-    external,
-    input: 'src/index.ts',
-    output: {
-      file: 'dist/index.mjs',
-      format: 'esm'
-    },
-    plugins: [
-      ...plugins,
-      typescript({
-        ...compilerOptions,
-        module: "ES2020",
-        moduleResolution: "node"
-      })
-    ]
-  }
+	{
+		external,
+		input,
+		output: {
+			dir: 'dist',
+			format: 'cjs',
+			entryFileNames: '[name].cjs',
+			chunkFileNames: '_chunks/[name]-[hash].cjs',
+		},
+		plugins: [...plugins, typescript(compilerOptions)],
+	},
+	{
+		external,
+		input,
+		output: {
+			dir: 'dist',
+			format: 'esm',
+			entryFileNames: '[name].mjs',
+			chunkFileNames: '_chunks/[name]-[hash].mjs',
+		},
+		plugins: [
+			...plugins,
+			typescript({
+				...compilerOptions,
+				module: 'ES2020',
+				moduleResolution: 'node',
+			}),
+		],
+	},
 ];
