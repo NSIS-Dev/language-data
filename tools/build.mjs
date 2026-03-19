f05ca4e6f0cc3332ff48e86714f6141b1a34429d
Jan T. Sott <git@idleberg.com>
2026-03-19 19:16:21 +0100

    chore: fix linter issues

--- tools/build.mjs
+++ tools/build.mjs
@@ -1,58 +1,60 @@
+import fs from 'node:fs';
+import { basename, extname, join } from 'node:path';
+import { promisify } from 'node:util';
 import MUI from '@nsis/mui-language-strings';
 import * as NLF from '@nsis/nlf';
-import fs from 'fs';
 import globby from 'globby';
-import { rimraf } from 'rimraf';
-import { basename, extname, join } from 'path';
 import { nsisDir } from 'makensis';
-import { promisify } from 'util';
+import { rimraf } from 'rimraf';
 
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 const mkdir = promisify(fs.mkdir);
 
-  try {
-    await rimraf('./data');
-    await mkdir('./data');
-    await mkdir('./data/languages');
-  } catch(e) {
-    throw new Error(e);
-  }
+try {
+	await rimraf('./data');
+	await mkdir('./data');
+	await mkdir('./data/languages');
+} catch (e) {
+	throw new Error(e);
+}
 
-  const output = {};
-  const languageDir = join(await nsisDir(), 'Contrib', 'Language files');
-  let languageFiles = await globby(`${languageDir}/*.nlf`);
+const output = {};
+const languageDir = join(await nsisDir(), 'Contrib', 'Language files');
+let languageFiles = await globby(`${languageDir}/*.nlf`);
 
-  languageFiles = languageFiles.sort();
+languageFiles = languageFiles.sort();
 
-   await Promise.all(languageFiles.map(async languageFile => {
-    const langExtname = extname(languageFile);
-    const langBasename = basename(languageFile, langExtname);
+await Promise.all(
+	languageFiles.map(async (languageFile) => {
+		const langExtname = extname(languageFile);
+		const langBasename = basename(languageFile, langExtname);
 
-    const nlfFile = await readFile(`${languageDir}/${langBasename}.nlf`, 'utf8');
-    const nlfContent = NLF.parse(nlfFile);
+		const nlfFile = await readFile(`${languageDir}/${langBasename}.nlf`, 'utf8');
+		const nlfContent = NLF.parse(nlfFile);
 
-    const nshFile = await readFile(`${languageDir}/${langBasename}.nsh`, 'utf8');
-    const nshContent = await MUI.parse(nshFile, { looseQuotes: true });
+		const nshFile = await readFile(`${languageDir}/${langBasename}.nsh`, 'utf8');
+		const nshContent = await MUI.parse(nshFile, { looseQuotes: true });
 
-    output[nshContent.file] = {
-      id: nlfContent.id,
-      code_page: nlfContent.code_page,
-      rtl: nlfContent.rtl,
-      english: nshContent.english,
-      native: nshContent.native,
-      native_ascii: nshContent.nativeASCII
-    };
+		output[nshContent.file] = {
+			id: nlfContent.id,
+			code_page: nlfContent.code_page,
+			rtl: nlfContent.rtl,
+			english: nshContent.english,
+			native: nshContent.native,
+			native_ascii: nshContent.nativeASCII,
+		};
 
-    try {
-      await writeFile(`data/languages/${nshContent.file}.json`, JSON.stringify(nlfContent, null, 2), 'utf8');
-    } catch (e) {
-      throw new Error(e);
-    }
-  }));
+		try {
+			await writeFile(`data/languages/${nshContent.file}.json`, JSON.stringify(nlfContent, null, 2), 'utf8');
+		} catch (e) {
+			throw new Error(e);
+		}
+	}),
+);
 
-  try {
-    await writeFile(`data/languages/${nshContent.file}.json`, JSON.stringify(nlfContent, null, 2), 'utf8');
-  } catch (e) {
-    await writeFile('data/meta.json', JSON.stringify(output, null, 2), 'utf8');
-  }
+try {
+	await writeFile(`data/languages/${nshContent.file}.json`, JSON.stringify(nlfContent, null, 2), 'utf8');
+} catch {
+	await writeFile('data/meta.json', JSON.stringify(output, null, 2), 'utf8');
+}
